40539af40013208801572f92f4ee52fb9d669298
Jigyansh sahu <indianinja008@gmail.com>
2026-03-23 18:42:56 +0530

    fix toaster

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-const page = async () => {
-  const queryclient = getQueryClient();
-  void queryclient.prefetchQuery(trpc.getUsers.queryOptions());
+"use client"
 
+
+const page = () => {
   return (
-    <div className=" flex h-screen justify-center items-center">
-      <HydrationBoundary state={dehydrate(queryclient)}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default page;
+export default page
