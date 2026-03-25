ae6095f67fe0b2238a845b26f2d2a2ab59edafbd
Jigyansh sahu <indianinja008@gmail.com>
2026-03-25 19:20:40 +0530

    fettching the data is working

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,10 @@
 import { RegisterForm } from '@/features/auth/components/register-form'
+import { requireUnAuth } from '@/lib/auth-utils'
 import React from 'react'
 
-const page = () => {
+const page = async() => {
+  await requireUnAuth();
+  
   return (
     <div>
       <RegisterForm/>
